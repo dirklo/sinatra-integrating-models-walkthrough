eafd3bcae5f230e399d711199e596582613ef082
Rick <dirklo@gmail.com>
2021-01-24 13:05:25 +0100

    commit tree a083ea871b996ef8b81d194c42b9bf7e2d92a2c2

--- app.rb
+++ app.rb
@@ -8,7 +8,12 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-
+    text = TextAnalyzer.new(text_from_user)
+    @word_count = text.word_counter
+    @vowel_count = text.vowel_counter
+    @consonant_count = text.consonant_counter
+    @most_common_letter = text.most_common_letter[0]
+    @common_letter_count = text.most_common_letter[1]
     erb :results
   end
 end
--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,34 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer 
+    attr_accessor :text
+
+    def initialize(text)
+        @text = text
+    end
+
+    def word_counter
+        text.split(" ").count
+    end
+
+    def vowel_counter
+        text.downcase.split("").filter{|l| ["a", "e", "i", "o", "u"].include?(l)}.count
+    end
+
+    def consonant_counter
+        text.gsub(" ", "").downcase.split("").filter{|l| !["a", "e", "i", "o", "u"].include?(l)}.count
+    end
+
+    def most_common_letter
+        array = text.gsub(" ", "").downcase.split("")
+        most_common_count = 0
+        most_common_letter = ""
+        array.each_with_index do |l, i|
+            current = array.count(array[i])
+            if current > most_common_count
+                most_common_count = current
+                most_common_letter = array[i]
+            end
+        end
+        [most_common_letter.upcase, most_common_count]
+    end
+end
